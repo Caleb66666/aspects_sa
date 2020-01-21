d28edc7834a172b87a77fb5873ae4d499cd00ad4
王千 <wangqian@mininglamp.com>
2020-01-21 16:34:17 +0800

    输出并追踪日志

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 model=albert_attn_pool
+out=nohup.out
+if [[ ! -f "$out" ]]; then
+    touch "$out"
+fi
+
 nohup python -u train_infer.py \
             --pattern train \
             --model "$model" \
             --seed 279 \
             &
-
+tail -f "$out"
