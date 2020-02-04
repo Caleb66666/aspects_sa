01d721ae200e2e05015d42ef3eaa18c2e401ac42
王千 <wangqian@mininglamp.com>
2020-02-04 12:31:51 +0800

    关闭debug模式

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 nohup python -u train_infer.py \
             --pattern train \
             --model "$model" \
-            --debug \
+#            --debug \
             --seed 279 \
             &
 tail -f "$out"
