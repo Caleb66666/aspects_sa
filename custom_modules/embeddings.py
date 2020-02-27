20438b492f2946d3fa67e50438db44eb8f93db03
王千 <wangqian@mininglamp.com>
2020-02-27 18:55:02 +0800

    完善word-char词嵌入，新增一种融合方式

--- custom_modules/embeddings.py
+++ custom_modules/embeddings.py
@@ -5,6 +5,7 @@
 
 import torch
 from torch import nn
+from custom_modules.fusions import Highway, SfuCombiner, BasicSfu, Concat
 
 
 class Squeezer(nn.Module):
@@ -66,12 +67,12 @@ class TransferEmbedding(nn.Module):
         [setattr(p, "requires_grad", False) for p in transfer_model.parameters()]
         if isinstance(embedding_attributes, str):
             embedding_attributes = (embedding_attributes,)
-        self.embedding = self.obtain_word_embedding(transfer_model, embedding_attributes)
+        self.embedding = self.obtain_token_embedding(transfer_model, embedding_attributes)
         if fine_tune_embed:
             [setattr(p, "requires_grad", True) for p in self.embedding.parameters()]
 
     @staticmethod
-    def obtain_word_embedding(transfer_model, embedding_attributes):
+    def obtain_token_embedding(transfer_model, embedding_attributes):
         embedding = transfer_model
         for attr in embedding_attributes:
             embedding = getattr(embedding, attr)
@@ -79,3 +80,123 @@ class TransferEmbedding(nn.Module):
 
     def forward(self, seq_ids, seq_len):
         return self.embedding(seq_ids)
+
+
+class WordCharEmbeddingWithCnn(nn.Module):
+    def __init__(self, word_vocab_size, word_embed_size, char_vocab_size, char_embed_size, n_channel, kernel_sizes,
+                 max_seq=1000, highway_layers=3, positional_encoding=False):
+        super().__init__()
+        self.positional_encoding = positional_encoding
+
+        self.word_embed_size = word_embed_size
+        self.word_embedding = nn.Embedding(word_vocab_size, self.word_embed_size)
+
+        self.char_embed_size = char_embed_size
+        self.char_embedding = nn.Embedding(char_vocab_size, self.char_embed_size)
+        self.char_vec_dim = n_channel * len(kernel_sizes)
+        self.cnn_list = nn.ModuleList([nn.Conv2d(1, n_channel, [ks, self.char_embed_size]) for ks in kernel_sizes])
+
+        self.embed_size = self.word_embed_size + self.char_vec_dim
+        self.highway = Highway(self.embed_size, num_layers=highway_layers)
+
+        if self.positional_encoding:
+            self.positional_embed = self.make_positional_encoding(self.embed_size, max_seq=max_seq)
+
+    @staticmethod
+    def make_positional_encoding(embed_size, max_seq):
+        pe = torch.arange(0, max_seq).unsqueeze(1).expand(max_seq, embed_size)
+        div_term = torch.pow(10000, torch.arange(0, embed_size * 2, 2) / embed_size)
+        pe /= div_term
+        pe[:, 0::2] = torch.sin(pe[:, 0::2])
+        pe[:, 1::2] = torch.cos(pe[:, 1::2])
+        return pe
+
+    def load_pre_trained_embeddings(self, pre_word_embedding=None, pre_char_embedding=None):
+        self.word_embedding.from_pretrained(torch.from_numpy(pre_word_embedding))
+        self.char_embedding.from_pretrained(torch.from_numpy(pre_char_embedding))
+
+    def obtain_word_embed(self, word_idx):
+        return self.word_embedding(word_idx)
+
+    def obtain_char_embed(self, char_idx):
+        batch_size, max_seq, max_char_len = char_idx.size()
+        char_idx = char_idx.view(-1, char_idx.size(2))
+        raw_char_embed = self.char_embedding(char_idx).unsqueeze(1)
+        char_embed = []
+        for cnn_unit in self.cnn_list:
+            cnn_raw_embed = cnn_unit(raw_char_embed).squeeze(-1)
+            pooled_cnn = torch.max_pool1d(cnn_raw_embed, cnn_raw_embed.size(-1)).squeeze(-1)
+            char_embed.append(pooled_cnn)
+        char_embed = torch.cat(char_embed, dim=-1)
+        char_embed = char_embed.view(batch_size, max_seq, -1)
+        print(char_embed.size())
+        return char_embed
+
+    def forward(self, word_idx, char_idx):
+        char_idx = char_idx.view(word_idx.size(0), word_idx.size(1), -1)
+
+        word_embed = self.obtain_word_embed(word_idx)
+        char_embed = self.obtain_char_embed(char_idx)
+        embedding = self.highway(torch.cat([word_embed, char_embed], dim=-1))
+
+        if self.positional_encoding:
+            positional_vector = nn.Parameter(self.positional_embed[:self.max_seq], requires_grad=True)
+            embedding += positional_vector
+
+        return embedding
+
+
+class WordCharEmbeddingWithRnn(nn.Module):
+    def __init__(self, word_vocab_size, word_embed_size, char_vocab_size, char_embed_size, rnn_hidden_size,
+                 rnn_layers=1, bidirectional=True, positional_encoding=False, fusion_method="cat"):
+        super().__init__()
+        self.positional_encoding = positional_encoding
+
+        self.word_embed_size = word_embed_size
+        self.word_embedding = nn.Embedding(word_vocab_size, self.word_embed_size)
+
+        self.char_embed_size = char_embed_size
+        self.char_embedding = nn.Embedding(char_vocab_size, self.char_embed_size)
+        self.char_bidirectional = bidirectional
+        self.char_encoder = nn.LSTM(self.char_embed_size, rnn_hidden_size, num_layers=rnn_layers,
+                                    bidirectional=self.char_bidirectional, batch_first=True)
+
+        if bidirectional:
+            char_hidden_size = 2 * rnn_hidden_size
+        else:
+            char_hidden_size = rnn_hidden_size
+
+        if fusion_method.lower() == "cat":
+            self.fusion_model = Concat()
+        elif fusion_method.lower() == "sfu":
+            self.fusion_model = BasicSfu(self.word_embed_size, char_hidden_size)
+        else:
+            raise TypeError(f"bad fusion method: {fusion_method}")
+
+    def obtain_word_embed(self, word_idx):
+        return self.word_embedding(word_idx)
+
+    @staticmethod
+    def char_pooling(tensor):
+        max_p = torch.max_pool1d(tensor.transpose(1, 2), tensor.size(1)).squeeze(-1)
+        return max_p
+
+    def obtain_char_embed(self, char_idx):
+        batch_size, max_seq, max_char_len = char_idx.size()
+        char_idx = char_idx.view(-1, char_idx.size(2))
+        raw_char_embed = self.char_embedding(char_idx)  # batch * max_seq, max_char_len, char_dim
+        char_hidden, _ = self.char_encoder(raw_char_embed)
+        char_pooled = self.char_pooling(char_hidden)
+        return char_pooled.view(batch_size, max_seq, -1)
+
+    def forward(self, word_idx, char_idx):
+        char_idx = char_idx.view(word_idx.size(0), word_idx.size(1), -1)
+
+        word_embed = self.obtain_word_embed(word_idx)  # batch_size, max_seq, word_embed_size
+        char_embed = self.obtain_char_embed(char_idx)  # batch_size, max_seq, 2 * char_hidden_size
+        embedding = self.fusion_model(word_embed, char_embed)
+        return embedding
+
+
+if __name__ == '__main__':
+    pass
--- custom_modules/fusions.py
+++ custom_modules/fusions.py
@@ -8,17 +8,35 @@ import torch
 from torch import nn
 
 
-class SFU(nn.Module):
+class BasicSfu(nn.Module):
     def __init__(self, input_size, fusion_size):
-        super(SFU, self).__init__()
+        super(BasicSfu, self).__init__()
         self.linear_r = nn.Linear(input_size + fusion_size, input_size)
         self.linear_g = nn.Linear(input_size + fusion_size, input_size)
 
-    def forward(self, raw_x, fusion_x):
-        r_f = torch.cat([raw_x, fusion_x], dim=2)
+    def forward(self, x, y):
+        r_f = torch.cat([x, y], dim=2)
         r = torch.tanh(self.linear_r(r_f))
         g = torch.sigmoid(self.linear_g(r_f))
-        o = g * r + (1 - g) * raw_x
+        o = g * r + (1 - g) * x
+        return o
+
+
+class SfuCombiner(nn.Module):
+    def __init__(self, input_size, fusion_size, dropout=None, training=True):
+        super(SfuCombiner, self).__init__()
+        self.linear_r = nn.Linear((input_size + fusion_size) * 2, input_size)
+        self.linear_g = nn.Linear((input_size + fusion_size) * 2, input_size)
+        self.dropout = dropout
+        self.training = training
+
+    def forward(self, x, y):
+        r_f = torch.cat([x, y, x * y, x - y], dim=2)
+        if self.dropout is not None:
+            r_f = nn.functional.dropout(r_f, p=self.dropout, training=self.training)
+        r = torch.tanh(self.linear_r(r_f))
+        g = torch.sigmoid(self.linear_g(r_f))
+        o = g * r + (1 - g) * x
         return o
 
 
@@ -29,3 +47,46 @@ class NoneLinearGate(nn.Module):
     def forward(self, raw_x, fusion_x):
         gate_x = torch.sigmoid(raw_x)
         return fusion_x * gate_x
+
+
+class Highway(nn.Module):
+    def __init__(self, input_size, num_layers, identity=True, activate_f=None):
+        super(Highway, self).__init__()
+        self.identity = identity
+
+        self.nonlinear = nn.ModuleList([nn.Linear(input_size, input_size) for _ in range(num_layers)])
+        if not self.identity:
+            self.linear = nn.ModuleList([nn.Linear(input_size, input_size) for _ in range(num_layers)])
+
+        self.gate = nn.ModuleList([nn.Linear(input_size, input_size) for _ in range(num_layers)])
+        self.activate = activate_f or nn.ReLU()
+
+    def forward(self, x):
+        """
+        Args:
+            x  (Tensor): [batch_size, size]
+        Return:
+            x' (Tensor): [batch_size, size]
+
+        Applies σ(x) ⨀ f(G(x)) + (1 - σ(x)) ⨀ Q(x)
+
+        linear: Q (affine transformation) or x (Identity)
+        nonlinear: f (non-linear tranformation) with G (affine transformation)
+        gate: σ(x) (affine transformation) with sigmoid
+        ⨀: element-wise multiplication
+        """
+        if self.identity:
+            for nonlinear, gate in zip(self.nonlinear, self.gate):
+                gate = torch.sigmoid(gate(x))
+                x = gate * self.activate(nonlinear(x)) + (1 - gate) * x
+        else:
+            for nonlinear, linear, gate in zip(self.nonlinear, self.linear, self.gate):
+                gate = torch.sigmoid(gate(x))
+                x = gate * self.activate(nonlinear(x)) + (1 - gate) * linear(x)
+
+        return x
+
+
+class Concat(nn.Module):
+    def forward(self, x, y, dim=-1):
+        return torch.cat([x, y], dim=dim)
